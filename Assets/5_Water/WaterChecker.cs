ab49ed2bd81c270e1639b4b821e990a0ca461814
thomasj52 <thomas.j.lezcano@gmail.com>
2026-06-26 16:28:08 -0300

    Organize

--- Assets/5_Water/WaterChecker.cs
+++ Assets/5_Water/WaterChecker.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class WaterChecker: MonoBehaviour
 {
     [SerializeField] private Material mat; // arrastrá el epicentro en el Inspector
 
