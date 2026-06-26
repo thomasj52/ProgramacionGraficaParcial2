d581975bbd77bf591902e584cf732da86521e05f
Augusto3737 <augustogalban@hotmail.com>
2026-06-26 18:04:46 -0300

    Build

--- Assets/AugustoGalban/SceneManager/PantallaFin.cs
+++ Assets/AugustoGalban/SceneManager/PantallaFin.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PantallaFin : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void OnExitClick()
     {
         SceneController.Instance.IrAMenu();
